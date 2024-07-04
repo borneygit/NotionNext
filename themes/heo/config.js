62377326c9a72343c4b4bce9a95ff92f765fe20d
borneygit <50075321+borneygit@users.noreply.github.com>
2024-07-04 14:13:52 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -15,7 +15,7 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享学习、编程、生活',
+  HEO_HERO_TITLE_1: '学海无涯,生活有度',
   HEO_HERO_TITLE_2: 'BORNEY.FUN',
   HEO_HERO_TITLE_3: '',
   HEO_HERO_TITLE_4: '',
